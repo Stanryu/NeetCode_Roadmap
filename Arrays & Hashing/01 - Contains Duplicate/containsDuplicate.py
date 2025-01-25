8b63629b36e92cc7ea2af45da1d89d5d3438bec2
Vinicius Marques do Nascimento <vin.1991@hotmail.com>
2025-01-25 17:41:35 -0300

    Arrays & Hashing - Contains Duplicate - python
    Arrays & Hashing - Contains Duplicate - python

---
+++ Arrays & Hashing/01 - Contains Duplicate/containsDuplicate.py
@@ -0,0 +1,15 @@
+import typing
+
+class Solution:
+    def hasDuplicate(self, nums: list[int]) -> bool:
+        nums_set = set(nums)
+        if len(nums) == len (nums_set):
+            return False
+        else:
+            return True
+
+
+if __name__ == "__main__":
+    a = Solution() 
+    nums = [1,2,3,4,6,7,8,9,5]
+    print(a.hasDuplicate(nums))

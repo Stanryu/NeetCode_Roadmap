288c99a45e0c0b53955d710604c56e58520a3728
Vinicius Marques do Nascimento <vin.1991@hotmail.com>
2025-01-26 17:23:27 -0300

    Arrays & Hashing - Valid Anagram - python
    Improving the code using hash.

--- Arrays & Hashing/02 - Valid Anagram/valid_anagram.py
+++ Arrays & Hashing/02 - Valid Anagram/valid_anagram.py
@@ -1,9 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = sorted(s)
-        t = sorted(t)
-        return s == t
+        len_s = len(s)
+        len_t = len(t)
+        if len_s != len_t:
+            return False
+        
+        count_s = {}
+        count_t = {}
+
+        for i in range(len_s):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
+
+        return count_s == count_t
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.isAnagram("rat", "car"))
+    print(sol.isAnagram("bbcc", "ccbc"))
